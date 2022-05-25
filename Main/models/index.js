1fb815281b1e080750e6c4496671e992a73d1683
bcorporal <bcorporal@gmail.com>
2022-05-25 01:09:08 -0400

    refactoring models

--- Main/models/index.js
+++ Main/models/index.js
@@ -1,10 +1,44 @@
 const User = require('./User');
 const Project = require('./Project');
+const Comment = require('./Comment');
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
+User.hasMany(Post, {
+  foreignKey: 'userId',
   onDelete: 'CASCADE'
 });
+
+User.hasMany(Comment, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'userId',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'postId',
+
+});
+
+module.exports = {
+  User,
+  Comment,
+  Post
+};
+
+
+
+
 
 Project.belongsTo(User, {
   foreignKey: 'user_id'
